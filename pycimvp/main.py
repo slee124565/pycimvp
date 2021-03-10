0c37d07c4bf489a23195cf95801c39bf35fc7658
Lee Shiueh <lee.shiueh@gmail.com>
2021-03-10 13:28:22 +0800

    refactoring

---
+++ pycimvp/main.py
@@ -0,0 +1,29 @@
+"""Demo Module
+
+A demo module example.
+
+"""
+from .__version__ import __title__
+from .__version__ import __description__
+from .__version__ import __url__
+from .__version__ import __version__
+from .__version__ import __author__
+from .__version__ import __author_email__
+from .__version__ import __license__
+
+
+def my_function() -> bool:
+    """A simple function for test coverage coveralls.io integration."""
+
+    print(f'__title__ {__title__}')
+    print(f'__description__ {__description__}')
+    print(f'__url__ {__url__}')
+    print(f'__version__ {__version__}')
+    print(f'__author_email__ {__author_email__}')
+    print(f'__author__ {__author__}')
+    print(f'__license__ {__license__}')
+    return True
+
+
+if __name__ == '__main__':
+    my_function()
